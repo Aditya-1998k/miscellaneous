f18b66e9418962d44fc128c03526aeacc5d11b60
aditya <aditya98gupta@gmail.com>
2023-08-19 21:56:15 +0530

    adding compress method for compressing data

--- compress.py
+++ compress.py
@@ -1,1 +1,17 @@
-print("compress")
+import zlib
+
+def compress(data):
+    """
+    args: string data to be compressed
+    return: compressed data
+    """
+    if not isinstance(data, str):
+        data = str(data)
+
+    compressed_data = zlib.compress(data.encode('utf-8'))
+
+    return compressed_data
+
+
+data_to_be_compressed ='Hello Wolrd'
+print(compress(data_to_be_compressed))
